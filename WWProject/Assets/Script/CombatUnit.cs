858dc6d212f604bc1837df71d124885281c5afff
Ruben <puurkool@gmail.com>
2016-07-07 01:36:59 +0200

    Scripts
    Added script folder and a basic combatunitscript

---
+++ WWProject/Assets/Script/CombatUnit.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum CombatClass { Warrior, Fighter};
+
+public class CombatUnit : MonoBehaviour {
+
+    public float maxHealth, currentHealth, strength, defense;
+    public CombatClass combatClass;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
