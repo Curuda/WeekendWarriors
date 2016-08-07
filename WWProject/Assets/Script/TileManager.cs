07ddf02f2a02af23019a31e5d1f7ffa184dd8582
Curuda <puurkool@gmail.com>
2016-08-07 12:13:09 +0200

    Working tiles in orthographic view
    also changed list to a 2d array for easier adjusting to A* later on

---
+++ WWProject/Assets/Script/TileManager.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TileManager : MonoBehaviour
+{
+
+    public GameObject tile;
+
+
+    TileScript[,] tileArray = new TileScript[10, 10];
+
+
+    TileScript SelectedTile;
+
+    // Use this for initialization
+    void Start()
+    {
+        for (int i = -5; i < 5; i++)
+        {
+            for (int j = -5; j < 5; j++)
+            {
+                GameObject spawnedTile = (GameObject)Instantiate(tile, new Vector3(i * 1.1f, 0, j * 1.1f), Quaternion.identity);
+                tileArray[i + 5, j + 5] = spawnedTile.GetComponent<TileScript>();
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            for (int j = 0; j < 10; j++)
+            {
+                if (tileArray[i, j] != SelectedTile)
+                    tileArray[i, j].tileType = TileType.Default;
+            }
+        }
+
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = 0;
+        Ray ray = Camera.main.ScreenPointToRay(mousePos);
+        Debug.Log(ray.direction);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 40))
+        {
+            if (hit.collider.gameObject.name.Contains("Tile"))
+            {
+                if (Input.GetMouseButtonDown(0))
+                {
+                    if (SelectedTile)
+                        SelectedTile.tileType = TileType.Default;
+                    SelectedTile = hit.collider.gameObject.GetComponent<TileScript>();
+                    SelectedTile.tileType = TileType.Selected;
+                }
+                else
+                {
+                    if (hit.collider.gameObject.GetComponent<TileScript>() != SelectedTile)
+                        hit.collider.gameObject.GetComponent<TileScript>().tileType = TileType.Hovered;
+                }
+            }
+        }
+    }
+}
---
+++ WWProject/Assets/Script/TileScript.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum TileType { Selected, Hovered, Default }
+
+public class TileScript : MonoBehaviour
+{
+
+    public Color defaultColour;
+    public Color hoverColour;
+    public Color selectedColour;
+
+    public TileType tileType;
+
+    [Range(0.0f, 10.0f)]
+    public float hello;
+
+    Material material;
+
+    // Use this for initialization
+    void Start()
+    {
+        material = GetComponent<Renderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (tileType == TileType.Selected)
+        {
+            material.SetColor("_Color", selectedColour);
+        }
+        else if (tileType == TileType.Hovered)
+        {
+            material.SetColor("_Color", hoverColour);
+        }
+        else if (tileType == TileType.Default)
+        {
+            material.SetColor("_Color", defaultColour);
+        }
+
+    }
+}
